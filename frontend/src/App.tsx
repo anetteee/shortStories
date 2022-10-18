50b2996b241c68577840405a21ac2c4840784da1
Tuva Djupvik <Tuva@dhcp-10-22-13-15.wlan.ntnu.no>
2022-10-18 10:29:42 +0200

    #3 saved app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { apolloClient } from "./components/apolloClient";
-import { gql, useQuery } from '@apollo/client';
-import { Post } from "./components/Post";
 import "./App.css"; 
 import HomePage from './components/HomePage';
 
@@ -11,26 +9,6 @@ export const App = () => (
     <HomePage/>
   </ApolloProvider>
 )
-
-interface PostQueryResponse {
-  posts: Post[];
-}
-
-const GET_POSTS = gql`
-  query {
-    posts {
-      _id: ID, 
-      id: Int,
-      quote: String, 
-      author: String,
-    }
-  }
-`;
-
-export function usePostsQuery() {
-  return useQuery<PostQueryResponse>(GET_POSTS);
-  
-}
 
 export default App;
 
