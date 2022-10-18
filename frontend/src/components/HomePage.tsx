0d8136392d467f6fc9ba76f8d451791509998a93
CamillaKK <camillakveimkopperud@gmail.com>
2022-10-18 14:04:16 +0200

    #4 Make query with basic filter
    Can get quote with speciefied id

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Query } from "../queries/Query";
+import QueryOnId from "../queries/QueryOnId";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Homepage</h1>
+      <QueryOnId />
       <Query />
     </div>
   );
