c98cf6688453c851c6c70056cc8561f0a80eefec
Anette Johansen <evaajo@stud.ntnu.no>
2022-10-18 11:43:08 +0200

    #3 delete unnecessary methods

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,25 +1,12 @@
-import React from "react";
-import { gql, useQuery } from '@apollo/client';
 import { Query } from "../queries/Query";
-  
-const GET_POSTS = gql`
-    query {
-        posts {
-        _id: ID, 
-        id: Int,
-        quote: String, 
-        author: String,
-        }
-    }
-`;
-  
+
 const HomePage = () => {
-    return (
+  return (
     <div>
       <h1>Hello World!</h1>
-      <Query/>
+      <Query />
     </div>
   );
-}
+};
 
 export default HomePage;
