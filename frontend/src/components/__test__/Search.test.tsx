f8126552c64f5183f0eae3cbbcff0eb41dc94fec
Anette Johansen <evaajo@stud.ntnu.no>
2022-11-01 18:00:44 +0100

    #19 make loading test and mock data

---
+++ frontend/src/components/__test__/Search.test.tsx
@@ -0,0 +1,51 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import { GET_POST_INVENTORY } from "../../queries/Queries";
+import { Search } from "../Search";
+
+const mocks = [
+  {
+    request: {
+      query: GET_POST_INVENTORY,
+      variables: {
+        tag: null,
+        limit: 10,
+        offset: 0,
+        keepPreviousData: true,
+        input: null,
+        sortBy: null,
+      },
+    },
+    result: {
+      data: {
+        getPost: {
+          posts: [
+            {
+              _id: "63525282dcb92aeaf8be3d59",
+              id: 5,
+              title: "Hopes and dreams were dashed that day.",
+              body: "Hopes and dreams were dashed that day. It should have been expected, but it still came as a shock. The warning signs had been ignored in favor of the possibility, however remote, that it could actually happen. That possibility had grown from hope to an undeniable belief it must be destiny. That was until it wasn't and the hopes and dreams came crashing down.",
+              userId: 41,
+              tags: ["crime", "mystery", "love"],
+              reactions: 4,
+            },
+          ],
+          count: 1,
+        },
+      },
+    },
+  },
+];
+
+it("renders without error", async () => {
+  render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <Search />
+    </MockedProvider>
+  );
+  expect(await screen.findByText("Loading...")).toBeInTheDocument();
+  //   expect(
+  //     await screen.findByText("Hopes and dreams were dashed that day.")
+  //   ).toBeInTheDocument();
+});
