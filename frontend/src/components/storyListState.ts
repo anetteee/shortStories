625bee56ffbdf65e8f1cbe18a6bed619da4f47f5
CamillaKK <camillakveimkopperud@gmail.com>
2022-11-03 14:55:54 +0100

    #22 Add comments to recoil state

--- frontend/src/components/storyListState.ts
+++ frontend/src/components/storyListState.ts
@@ -1,6 +1,7 @@
 import { atom } from "recoil";
 
+//The atom represents the state
 export const expandedStoriesListState = atom<String[]>({
-    key: "storyState",
-    default: [],
+    key: "storyState", //Unique identifier internally
+    default: [], //Initial value of atom
   });
